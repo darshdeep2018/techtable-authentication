64551dd8c47000971d453ba1ae9472ca0a3d6725
Darshdeep Singh <d.s.sidhu2017@gmail.com>
2020-06-20 11:51:47 +0530

    second commit

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ passport.use(new GoogleStrategy({
     callbackURL: "/auth/google/callback"
   },
   (accessToken, refreshToken, profile, done) =>{
-    //User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      //return done(err, user);
-    //});
-    console.log('done');
+    console.log('profile',profile)
+    console.log('done',done);
   }
 ));
 
@@ -22,28 +20,6 @@ app.get('/auth/google',
 
 app.get('/auth/google/callback', 
   passport.authenticate('google'));
-
-
-// app.use(
-//     cookieSession({
-//         maxAge:30 * 24 * 60 * 60 *1000,
-//         keys:[keys.cookieKey]
-//     })
-// )
-
-// app.use(passport.initialize());
-// app.use(passport.session())
-
-// require('./routes/authRoute')(app)
-
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     const path = require('path');
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
-
 
 app.listen(PORT,()=>{
     console.log("server running on "+ PORT)
